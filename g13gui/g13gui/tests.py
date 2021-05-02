f257d8f11debf25a33f33c05d9224ccde70a0786
June Tate-Gans <june@theonelab.com>
2021-05-02 12:52:37 -0500

    g13gui: Add a toplevel unittest module
    This gives us a one-stop-shop for running unit tests.

---
+++ g13gui/g13gui/tests.py
@@ -0,0 +1,13 @@
+import unittest
+
+from g13gui.observer.observer_tests import *
+from g13gui.model.bindingprofile_tests import *
+from g13gui.model.prefs_tests import *
+from g13gui.bitwidgets.fonts_tests import *
+from g13gui.bitwidgets.label_tests import *
+from g13gui.bitwidgets.display_tests import *
+from g13gui.bitwidgets.screen_tests import *
+
+
+if __name__ == '__main__':
+    unittest.main()
