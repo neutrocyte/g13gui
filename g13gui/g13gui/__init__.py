dc7f9211ec70a29d180d545d58ed6a8b48c6269f
June Tate-Gans <june@theonelab.com>
2021-05-02 12:44:17 -0500

    g13gui: Stop importing stuff into toplevel namespaces
    This causes import errors later on as things get tangled.

--- g13gui/g13gui/__init__.py
+++
@@ -1,3 +0,0 @@
-import g13gui.model as model
-import g13gui.ui as ui
-import g13gui.observer as observer
--- g13gui/g13gui/bitwidgets/__init__.py
+++
@@ -1,4 +0,0 @@
-from g13gui.bitwidgets.fonts import Fonts
-from g13gui.bitwidgets.fonts import FontManager
-from g13gui.bitwidgets.display import Display
-from g13gui.bitwidgets.display import DisplayMetrics
