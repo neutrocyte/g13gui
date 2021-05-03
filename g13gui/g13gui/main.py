4d11270a7caeb3f510e6922001514ab2797b6cb0
June Tate-Gans <june@theonelab.com>
2021-05-03 18:10:56 -0500

    main: Since we use lots of crazy threads, ensure GDK is ready

--- g13gui/g13gui/main.py
+++ g13gui/g13gui/main.py
@@ -8,10 +8,12 @@ from g13gui.model.prefsstore import PreferencesStore
 from g13gui.g13.manager import Manager
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GObject
+from gi.repository import Gtk, Gdk, GObject
 
 
 if __name__ == '__main__':
+    Gdk.threads_init()
+
     prefs = PreferencesStore.getPrefs()
     manager = Manager(prefs)
     manager.start()
