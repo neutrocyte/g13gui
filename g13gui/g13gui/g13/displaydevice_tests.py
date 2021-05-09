8390c065fe5b5a3b6c90daeb338072e5cf07a6a3
June Tate-Gans <june@theonelab.com>
2021-05-08 19:27:46 -0500

    displaydevice: Ensure we start the DBus loop for DeviceManager

--- g13gui/g13gui/g13/displaydevice_tests.py
+++ g13gui/g13gui/g13/displaydevice_tests.py
@@ -1,20 +1,25 @@
 import unittest
 import time
 
+from dbus.mainloop.glib import DBusGMainLoop
+
 from g13gui.model.prefs import Preferences
 from g13gui.bitwidgets.display import Display
 from g13gui.bitwidgets.screen import Screen
 from g13gui.bitwidgets.label import Label
+from g13gui.bitwidgets.rectangle import Rectangle
 from g13gui.g13.displaydevice import G13DisplayDevice
-from g13gui.g13.manager import Manager
+from g13gui.g13.manager import DeviceManager
+from g13gui.bitwidgets import DISPLAY_WIDTH
+from g13gui.bitwidgets import DISPLAY_HEIGHT
 
 
 class DisplayDeviceTests(unittest.TestCase):
     def setUp(self):
         self.prefs = Preferences()
-        self.manager = Manager(self.prefs)
+        self.manager = DeviceManager(self.prefs)
         self.manager.start()
-        time.sleep(0.25)
+        time.sleep(1)
         self.dd = G13DisplayDevice(self.manager)
         self.d = Display(self.dd)
         self.s = Screen(self.d)
@@ -24,11 +29,18 @@ class DisplayDeviceTests(unittest.TestCase):
         self.manager.shutdown()
 
     def testDisplay(self):
+        rect = Rectangle(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        rect.show()
+        self.s.addChild(rect)
+
         label = Label(0, 0, 'Hello, world!')
         label.show()
         self.s.addChild(label)
+
+        self.s.buttonBar.hide()
         self.s.nextFrame()
 
 
 if __name__ == '__main__':
+    DBusGMainLoop(set_as_default=True)
     unittest.main()
