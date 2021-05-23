b6a0bf9a7cdfb4c005effb55602764793fb8859d
June Tate-Gans <june@theonelab.com>
2021-05-23 13:14:03 -0500

    g13gui: Swap to using appdirs

--- g13gui/common.py
+++ g13gui/common.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-from xdg import xdg_config_home
+from pathlib import Path
+from appdirs import user_config_dir
 
 PROGNAME = 'g13gui'
 VERSION = '0.1.0'
-PROFILES_CONFIG_PATH = xdg_config_home() / 'g13' / 'g13gui' / 'profiles.json'
+PROFILES_CONFIG_PATH = Path(user_config_dir()) / 'g13' / 'g13gui' / 'profiles.json'
