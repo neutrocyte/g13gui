5481bbff226b4594d15bedbd660c4784b850b701
June Tate-Gans <june@theonelab.com>
2021-05-23 11:32:25 -0500

    setup: Clean up the classifiers, requirements, and data files
    We have a bunch more info that is nearly correct, though I haven't validated
    this yet. Currently exploring using pipenv instead, since that helps manage
    these more effectively.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+
 from setuptools import setup, find_packages
 from os import path
 from io import open
+
 
 here = path.abspath(path.dirname(__file__))
 
@@ -17,24 +20,42 @@ setup(
     author='June Tate-Gans',
     author_email='june@theonelab.com',
     license='MIT',
-    classifiers = [
+    classifiers=[
         'Development Status :: 3 - Alpha',
-        'Topic :: Utilities',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Games/Entertainment',
         'License :: OSI Approved :: MIT',
         'Operating System :: POSIX',
         'Programming Language :: Python :: 3',
+        'Operating System :: Linux'
     ],
     keywords='gaming',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     python_requires='>=3.5.0',
     install_requires=[
-        'gi',
+        'PyGObject',
+        'PIL',
+        'Xlib',
+        'dbus',
+        'evdev',
+        'gzip',
+        'usb'
     ],
-    package_data={
+    data_files={
+        'share/applications': [
+            path.join(here, 'com.theonelab.g13gui.Configurator.desktop'),
+            path.join(here, 'com.theonelab.g13gui.applet.Clock.desktop'),
+            path.join(here, 'com.theonelab.g13gui.applet.Profiles.desktop'),
+        ],
+        'share/icons/hicolor/scalable/apps': [
+            path.join(here, 'g13-logo.svg')
+        ]
     },
     entry_points={
         'console_scripts': [
             'g13gui=g13gui.main:main',
+            'g13-clock=g13gui.applets.clock:',
+            'g13-profiles=g13gui.applets.profiles:'
         ],
     },
 )
